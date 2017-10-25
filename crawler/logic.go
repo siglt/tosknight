048172131fe1fa082625ed98d618871611c496c4
Ce Gao <ce.gao@outlook.com>
2017-10-25 17:59:07 +0800

    *: Add git support
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- crawler/logic.go
+++ crawler/logic.go
@@ -1,10 +1,12 @@
 package crawler
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"github.com/asciimoo/colly"
-	"github.com/gaocegege/tosknight/util"
+	"github.com/gaocegege/tosknight/git"
+	"github.com/siglt/tosknight/util"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,9 +15,11 @@ func parseResponse(response *colly.Response) {
 		log.Errorf("The status code of the response %v is %d", response.Request.AbsoluteURL, response.StatusCode)
 	}
 
+	gitManager := git.NewManager(StoragePath)
 	directory := filepath.Join(StoragePath, util.GetFileName(response.Request.URL.String()))
 	bufFilePath := filepath.Join(directory, util.BufFileName)
 	latestfilePath := filepath.Join(directory, util.LatestFileName)
+
 	util.MkDir(directory)
 	_, err := util.SaveFile(response.Body, bufFilePath)
 	if err != nil {
@@ -29,6 +33,7 @@ func parseResponse(response *colly.Response) {
 		if err != nil {
 			log.Errorf("Failed to save content to file %s: %v", latestfilePath, err)
 		}
+		gitManager.AddAndCommit(fmt.Sprintf("%s: First Commit", response.Request.URL))
 		return
 	}
 
@@ -37,10 +42,12 @@ func parseResponse(response *colly.Response) {
 		log.Errorf("Failed to check if the content is modified: %v", err)
 	}
 	if isModified == true {
-		persistentFilePath := filepath.Join(directory, util.PersistentFileName())
+		persistentFileName := util.PersistentFileName()
+		persistentFilePath := filepath.Join(directory, persistentFileName)
 		log.Debugf("The content is changed, write to %s", persistentFilePath)
 		util.CopyFile(latestfilePath, persistentFilePath)
 		util.CopyFile(bufFilePath, latestfilePath)
+		gitManager.AddAndCommit(fmt.Sprintf("%s: Update %s", response.Request.URL, persistentFileName))
 	} else {
 		log.Infof("There is no content changed in %s", response.Request.URL.String())
 	}
